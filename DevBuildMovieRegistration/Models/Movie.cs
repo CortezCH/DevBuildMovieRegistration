24daba4bae54a47a165523ddafd27a5d8bf00337
Cortez Christian <CortezChristian@rocketmortgage.com>
2021-12-02 09:33:06 -0500

    Initial Code push with complete website

---
+++ DevBuildMovieRegistration/Models/Movie.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevBuildMovieRegistration.Models
+{
+    public class Movie
+    {
+        public string ID { get; set; }
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public int Year { get; set; }
+        public string Actor { get; set; }
+        public string Director { get; set; }
+
+
+    }
+}
